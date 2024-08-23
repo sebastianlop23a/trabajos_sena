983d95c5db9cc3d468b0f49be67b5961aed86a73
sebastianlop23a <159330767+sebastianlop23a@users.noreply.github.com>
2024-08-22 21:34:22 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+numero1 = int(input("ingrese el primer numero a multiplicar:"))
+numero2 = int(input("ingrese el segundo numero a multiplicar:"))
+numero3 = int(input("ingrese el tercer numero a multiplicar:"))
+
+numerofinal=numero1*numero2*numero3 
+print("el resultado de la multiplicacion es:",numerofinal)
+
+
