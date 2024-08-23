18fe18475991b0a200b78da66eb34e82acba55ba
sebastianlop23a <159330767+sebastianlop23a@users.noreply.github.com>
2024-08-22 22:36:21 -0500

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,11 @@
+
+
+# x es la distanca recorrida
+# v es la velocidad
+# t es el tiempo
+
+velocidad= int(input("Ingrese la velocidad en Km/h: "))
+tiempo= int(input("Ingrese el tiempo en horas: "))
+
+distancia_rec = velocidad * tiempo
+print("La distancia recorrida es: ", distancia_rec, "Km")
